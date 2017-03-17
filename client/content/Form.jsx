36f6040788ab4a401a3b4ff1ac2532106dfa5165
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2017-03-17 18:29:13 +0000

    thank you message

--- client/content/Form.jsx
+++ client/content/Form.jsx
@@ -6,7 +6,7 @@ class Form extends Component {
     email: '',
     message: '',
     formSent: false,
-    thankYouMessage: "Thank you ! Your message was sent! 👏"
+    thankYouMessage: "Thank you! Your are a 🌟. Your message was sent! 👏"
   }
 
   onChange = (e) => {
@@ -17,20 +17,34 @@ class Form extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+      formSent: true,
+    })
   }
 
   render() {
     return (
       <div className="Form">
-        <form onSubmit={this.onSubmit}>
-          <label>Name</label>
-          <input type="text" value={this.state.name} data-id="name" onChange={this.onChange}/>
-          <label>Email</label>
-          <input type="text" value={this.state.email} data-id="email" onChange={this.onChange}/>
-          <label>Message</label>
-          <textarea type="text" value={this.state.message} data-id="message" onChange={this.onChange} rows="5" cols="60" placeholder="Tell us how you can help, please?"/>
-          <button onClick={this.onSubmit}>Send Message</button>
-        </form>
+        { this.state.formSent === false
+          ? <div>
+              <h1>Contact Maciej to help Marwan or scroll down to read the full story.</h1>
+              <form onSubmit={this.onSubmit}>
+                <label>Name</label>
+                <input type="text" value={this.state.name} data-id="name" onChange={this.onChange}/>
+                <label>Email</label>
+                <input type="text" value={this.state.email} data-id="email" onChange={this.onChange}/>
+                <label>Message</label>
+                <textarea type="text" value={this.state.message} data-id="message" onChange={this.onChange} rows="5" cols="60" placeholder="Tell us how you can help, please?"/>
+                <button onClick={this.onSubmit}>Send Message</button>
+              </form>
+            </div>
+          : <div className="ThankYou">
+            <h1>{this.state.thankYouMessage}</h1>
+          </div>
+        }
       </div>
     );
   }
